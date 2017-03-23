73cff09d7f435c0ee464d2abfe04e2d65bda7732
wangmei <18772382251@163.com>
2017-03-23 17:59:57 +0800

    注册页面添加表单校验

--- src/main/webapp/WEB-INF/pages/app/register.jsp
+++ src/main/webapp/WEB-INF/pages/app/register.jsp
@@ -4,8 +4,14 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="resources/css/plugins/font-awesomer/font-awesome.min.css" />
 <link rel="stylesheet" href="resources/css/plugins/bootstrap/bootstrap.css" />
+<link rel="stylesheet" href="resources/css/plugins/bootstrap/bootstrapValidator.min.css" />
 <link rel="stylesheet" href="resources/css/app/register.css" />
+<script type="text/javascript" src="resources/js/component/jquery/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="resources/js/component/bootstrap/bootstrap-3.3.5.min.js"></script>
+<script type="text/javascript" src="resources/js/component/bootstrap/bootstrapValidator.min.js"></script>
+<script type="text/javascript" src="resources/js/app/register.js"></script>
 <title>图书管理系统注册页面</title>
 </head>
 <body>
@@ -14,24 +20,27 @@
 		<div class="col-lg-6 register">
 			<h2 class="title">注 册</h2>
 			<form id="registerForm" class="registerFormStyle">
-				<div>
+				<div class="form-group">
 					<input type="text" class="comInput" id="username" name="username" placeholder="用户名"/>
 				</div>
-				<div>
+				<div class="form-group">
 					<input type="tel" class="comInput" id="phone" name="phone" placeholder="手机号"/>
 				</div>
-				<div>
+				<div class="form-group">
 					<input type="text" class="comInput" id="studentId" name="studentId" placeholder="学   号"/>
 				</div>
-				<div>
+				<div class="form-group">
 					<input type="password" class="comInput" id="password1" name="password1" placeholder="初始密码"/>
 				</div>
-				<div>
+				<div class="form-group">
 					<input type="password" class="comInput" id="password2" name="password2" placeholder="确认密码"/>
 				</div>
 				
-				<button class="btn btn-primary submit" value="register" type="submit">注  册</button>
-				<button class="btn btn-info cancel" value="cancel" type="reset">重  置</button>
+				<div class="buttons">
+					<button class="btn btn-primary submit" value="register" type="submit">注  册</button>
+					<button class="btn btn-info cancel" value="cancel" type="reset">重  置</button>
+				</div>
+				
 			</form>
 		</div>
 		<div class="col-lg-3"></div>
---
+++ src/main/webapp/resources/js/app/register.js
@@ -0,0 +1,76 @@
+/**
+ * 注册页面js
+ */
+$(document).ready(function() {
+	$("#registerForm").bootstrapValidator({
+        //字段验证
+        fields: {
+            //用户名
+            username:{
+                validators: {
+                    //非空
+                    notEmpty: {
+                        message: '*用户名不能为空'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 18,
+                        message: '*用户名长度必须在6到18位之间'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: '*用户名只能包含大写、小写、数字和下划线'
+                    }
+                }
+            },
+            phone:{
+                validators: {
+                    //非空
+                    notEmpty: {
+                        message: '*电话号码不能为空'
+                    },
+                    stringLength: {  
+                        min: 11,  
+                        max: 11,  
+                        message: '请输入11位手机号码'  
+                    },  
+                    regexp: {  
+                        regexp: /^1[3|5|8]{1}[0-9]{9}$/,  
+                        message: '请输入正确的手机号码'  
+                    }
+                }
+            },
+            studentId:{
+                validators: {
+                    //非空
+                    notEmpty: {
+                        message: '*学号不能为空'
+                    },
+                    numeric: {
+                    	message: '*学号只能输入数字'
+                    }
+                }
+            },
+            password1:{
+                validators: {
+                    //非空
+                    notEmpty: {
+                        message: '*密码不能为空'
+                    }
+                }
+            },
+            //密码
+            password2:{
+                validators: {
+                    notEmpty: {
+                        message: '*密码不能为空'
+                    },
+                    identical: {
+	                    field: 'password1',
+	                    message: '*两次密码不相同'
+	                }
+                }
+            }
+        }
+    })
+});
