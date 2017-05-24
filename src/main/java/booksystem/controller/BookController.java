d6d1acf0d5c0ec0eda0d17676426f0887c9f7940
Administrator <Administrator@ZGC-20170319QSL>
2017-05-24 15:39:30 +0800

    添加注释

--- src/main/java/booksystem/controller/BookController.java
+++ src/main/java/booksystem/controller/BookController.java
@@ -28,6 +28,10 @@ public class BookController {
 	@Resource
 	private BookService bookService;
 	
+	/**
+	 * 首页图书信息统计
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping(value = "bookInfo",method = RequestMethod.POST)
 	public List<UtilEntity> bookInfo() {
@@ -56,6 +60,13 @@ public class BookController {
 		return utilList;
 	}
 	
+	/**
+	 * 借书表图书信息统计
+	 * @param book
+	 * @param limit
+	 * @param offset
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping(value = "book",method = RequestMethod.GET)
 	public String book(Book book,@RequestParam(defaultValue="0",required=false)int limit,@RequestParam(defaultValue="0",required=false)int offset) {
